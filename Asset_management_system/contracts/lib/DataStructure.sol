50ada9b67565acf8e08100c7713606f2b5fdead4
Haripandey21 <iamgoing1214@gmail.com>
2023-06-25 14:41:09 +0545

    remove typo

--- Asset_management_system/contracts/lib/DataStructure.sol
+++ Asset_management_system/contracts/lib/DataStructure.sol
@@ -13,13 +13,6 @@ contract DataStructure{
         uint256 quantity;
         uint256 buying_price;
         bool exists;
-        // float is currently not supported in solidity, so to input quantity,
-        // input its value by multiplying it with 1000, and for buying_price
-        // multiply it with 100000.
-
-        // you may be wondering what if firm is investing in safemars whose value is 0.0000000012,
-        // i don't think any good campany would invest in shit coins with your money. we will later handle
-        //  it with front-end to show you the exact quantity and buying_price.
     } 
     
      struct tokenDetails {
